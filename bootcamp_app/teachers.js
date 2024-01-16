b6cf15b8ffbda0badfc10f9d0e70798322ed8a77
Lalika31253 <Lalika31253@gmail.com>
2024-01-16 12:23:51 -0700

    Made connection to a PostgreSQL database using the node-postgres library and write some queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+// -- Get the total number of assistance_requests for a teacher
+
+pool.query(`
+SELECT teachers.name as teacher, COUNT(assistance_requests.*) as total_assistances, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+GROUP BY teacher, cohorts.name;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
